a7e4b13bf83c49deb2822d154eb3223efbc47762
Ankit <ankitkumardmk98@gmail.com>
2025-01-11 21:05:20 +0530

    ToDo App first version

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color.fromARGB(255, 57, 19, 104);
+const appbarBgColor = Color.fromARGB(255, 243, 243, 243);
+const white = Color.fromARGB(255, 255, 255, 255);
+const black = Color.fromARGB(255, 39, 39, 39);
+const grey = Color.fromARGB(255, 187, 187, 187);
+const red = Color.fromARGB(255, 224, 33, 33);
---
+++ lib/main_page.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'constant.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class Task {
+  String taskTitle;
+  bool isDone;
+
+  Task({
+    required this.taskTitle,
+    this.isDone = false,
+  });
+}
+
+class _MainPageState extends State<MainPage> {
+  TextEditingController userInput = TextEditingController();
+
+// Function to show the alert
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Delete All"),
+          content: Text("Do You want to delete all your tasks?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                deleteAllTask();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      "All Tasks are cleared now",
+                      style: TextStyle(
+                        color: black,
+                      ),
+                    ),
+                    showCloseIcon: true,
+                    closeIconColor: black,
+                    duration: Duration(seconds: 2),
+                    backgroundColor: appbarBgColor,
+                  ),
+                );
+              },
+              child: Text(
+                "Yes",
+                style: TextStyle(
+                  color: red,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("No"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  List<Task> taskList = [];
+
+  void deleteAllTask() {
+    setState(() {
+      taskList.clear();
+    });
+  }
+
+  void addTask() {
+    if (userInput.text.isNotEmpty) {
+      taskList.add(
+        Task(taskTitle: userInput.text),
+      );
+      setState(() {
+        userInput.clear();
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            "Please add any task",
+            style: TextStyle(
+              color: black,
+            ),
+          ),
+          showCloseIcon: true,
+          closeIconColor: black,
+          duration: Duration(seconds: 2),
+          backgroundColor: appbarBgColor,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: appbarBgColor,
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "ToDo App",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _showAlert(context);
+                  },
+                  icon: Icon(Icons.delete),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: taskList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(
+                    taskList[index].taskTitle,
+                    style: TextStyle(
+                      color: grey,
+                      decoration: taskList[index].isDone
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(
+                      taskList[index].isDone
+                          ? Icons.check_box
+                          : Icons.check_box_outline_blank,
+                      color: grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        taskList[index].isDone = !taskList[index].isDone;
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                style: TextStyle(color: grey),
+                controller: userInput,
+                decoration: InputDecoration(
+                  hintText: "Add Your Task",
+                  hintStyle: TextStyle(
+                    color: grey,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: addTask,
+              icon: Icon(
+                Icons.add,
+                color: grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
